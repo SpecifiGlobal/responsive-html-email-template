81d0399052c8772a27548264d171fc65e01bf65d
salaros <salaros@yandex.ru>
2018-02-02 21:22:27 +0300

    inline CSS rules in HTML file via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
+var inlineCss = require('gulp-inline-css');
+
+// Inline CSS rules in HTML file
+gulp.task('inlinecss', [ 'inlinesource' ], function() {
+    return gulp.src('./out/*.html')
+        .pipe(inlineCss({ preserveMediaQueries: true }))
+        .pipe(gulp.dest('./out'));
+});
 
 // inline styles, javascript etc via Gulp
 gulp.task('inlinesource', function () {
