597ce44d72ad951d47f9e1f84a43435d645d36aa
salaros <salaros@yandex.ru>
2018-02-02 21:53:28 +0300

    adding SCSS to CSS compilation via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
+var scss = require("gulp-scss");
 var inlinesource = require('gulp-inline-source');
 var inlineCss = require('gulp-inline-css');
 
 // Inline CSS rules in HTML file
-gulp.task('inlinecss', [ 'inlinesource' ], function() {
+gulp.task('inlinecss', [ 'scss', 'inlinesource' ], function() {
     return gulp.src('./out/*.html')
         .pipe(inlineCss({ preserveMediaQueries: true }))
         .pipe(gulp.dest('./out'));
@@ -11,11 +12,16 @@ gulp.task('inlinecss', [ 'inlinesource' ], function() {
 
 // inline styles, javascript etc via Gulp
 gulp.task('inlinesource', function () {
-    var options = {
-        compress: false
-    };
-
     return gulp.src('./src/*.html')
-        .pipe(inlinesource(options))
+        .pipe(inlinesource({
+            compress: false
+        }))
         .pipe(gulp.dest('./out'));
+});
+
+// SCSS to CSS compilation
+gulp.task("scss", function () {
+    gulp.src("./css/*.scss")
+        .pipe(scss({"bundleExec": false}))
+        .pipe(gulp.dest("./css"));
 });
