f30dc288a3aff8c8fbf59d9baabddeb9d0409702
salaros <salaros@yandex.ru>
2018-02-02 22:00:00 +0300

    -

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp');
-var scss = require("gulp-scss");
-var inlinesource = require('gulp-inline-source');
-var inlineCss = require('gulp-inline-css');
+var gulp = require('gulp'),
+    scss = require("gulp-scss"),
+    inlinesource = require('gulp-inline-source'),
+    inlineCss = require('gulp-inline-css');
 
 gulp.task('default', [ 'inlinecss' ]);
 
 // Inline CSS rules in HTML file
-gulp.task('inlinecss', [ 'scss', 'inlinesource' ], function() {
+gulp.task('inlinecss', [ 'inlinesource' ], function() {
     return gulp.src('./out/*.html')
         .pipe(inlineCss({ preserveMediaQueries: true }))
         .pipe(gulp.dest('./out'));
 });
 
 // inline styles, javascript etc via Gulp
-gulp.task('inlinesource', function () {
+gulp.task('inlinesource', [ 'scss' ], function () {
     return gulp.src('./src/*.html')
         .pipe(inlinesource({
             compress: false
@@ -23,7 +23,7 @@ gulp.task('inlinesource', function () {
 
 // SCSS to CSS compilation
 gulp.task("scss", function () {
-    gulp.src("./css/*.scss")
+    return gulp.src("./css/*.scss")
         .pipe(scss({"bundleExec": false}))
         .pipe(gulp.dest("./css"));
 });
